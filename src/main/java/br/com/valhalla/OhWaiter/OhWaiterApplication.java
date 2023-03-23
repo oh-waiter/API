e95c977ab62d6dfd61c2b27514b4792e42610eaa
Matheus Ribeiro <matheus.junio159@gmail.com>
2023-03-23 11:40:38 -0300

    First Commit

---
+++ src/main/java/br/com/valhalla/OhWaiter/OhWaiterApplication.java
@@ -0,0 +1,13 @@
+package br.com.valhalla.OhWaiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhWaiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhWaiterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/valhalla/OhWaiter/OhWaiterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.valhalla.OhWaiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhWaiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
