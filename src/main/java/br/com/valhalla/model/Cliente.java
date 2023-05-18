30a0ea2996e5d93a3a404c4bb5baa012f75219a1
João Nicholas <joaonicholasfigueiredo@gmail.com>
2023-05-18 19:06:13 -0300

    Primeiro commit model

---
+++ src/main/java/br/com/valhalla/model/Cliente.java
@@ -0,0 +1,36 @@
+package br.com.valhalla.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long clienteID;
+
+    @Column(name = "NomeCliente", nullable = false, length = 100)
+    private String nomeCliente;
+
+    @Column(name = "CPFCliente", nullable = false, length = 20)
+    private String CPFCliente;
+
+
+}
---
+++ src/main/java/br/com/valhalla/model/Mesa.java
@@ -0,0 +1,30 @@
+package br.com.valhalla.model;
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Mesa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long mesaID;
+
+    @Column(name = "QuantidadeAssentos", nullable = false, length = 20)
+    private int quantidadeAssentos;
+
+    @Column(name = "TaxaDeReserva", nullable = false, length = 20)
+    private Double disponibilidadeMesa;
+
+    @ManyToOne
+    @JoinColumn(name = "ReservaID")
+    private Reserva reservaID;
+}
