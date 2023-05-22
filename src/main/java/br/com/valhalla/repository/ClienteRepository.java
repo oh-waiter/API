07e35b4ad6a53c33b26aa997a6002d1c95bdcf53
JoaoNicholasFS <joaonicholasfigueiredo@gmail.com>
2023-05-21 21:46:27 -0300

    Commit da API Cardapio

--- src/main/java/br/com/valhalla/repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.valhalla.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import br.com.valhalla.model.Cliente;
-
-public interface ClienteRepository extends JpaRepository<Cliente,Long>{
-}
--- src/main/java/br/com/valhalla/repository/ReservaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.valhalla.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import br.com.valhalla.model.Reserva;
-
-public interface ReservaRepository extends JpaRepository<Reserva,Long>{
-}
